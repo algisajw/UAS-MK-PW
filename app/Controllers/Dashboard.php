42a7ead08b0f0659c60ab340bf9973d0cb121aee
Algis Asmaradhana JW <gisssss.jw@gmail.com>
2026-05-14 21:45:40 +0800

    chore: mengisi code dalam file dashboard.php

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -1,1 +1,61 @@
 
+<?php
+
+namespace App\Controllers;
+
+use App\Models\GedungModel;
+
+class Dashboard extends BaseController
+{
+    public function index()
+    {
+        $model = new GedungModel();
+        $data['gedung'] = $model->findAll();
+        return view('dashboard/index', $data);
+    }
+
+    public function create()
+    {
+        return view('dashboard/create');
+    }
+
+    public function store()
+    {
+        $model = new GedungModel();
+        $data = [
+            'nama_gedung' => $this->request->getPost('nama_gedung'),
+            'kapasitas' => $this->request->getPost('kapasitas'),
+            'harga' => $this->request->getPost('harga'),
+            'lokasi' => $this->request->getPost('lokasi')
+        ];
+        $model->save($data);
+        return redirect()->to('/dashboard');
+    }
+
+    public function edit($id)
+    {
+        $model = new GedungModel();
+        $data['gedung'] = $model->find($id);
+        return view('dashboard/edit', $data);
+    }
+
+    public function update($id)
+    {
+        $model = new GedungModel();
+        $data = [
+            'nama_gedung' => $this->request->getPost('nama_gedung'),
+            'kapasitas' => $this->request->getPost('kapasitas'),
+            'harga' => $this->request->getPost('harga'),
+            'lokasi' => $this->request->getPost('lokasi')
+        ];
+        $model->update($id, $data);
+        return redirect()->to('/dashboard');
+    }
+
+    public function delete($id)
+    {
+        $model = new GedungModel();
+        $model->delete($id);
+        return redirect()->to('/dashboard');
+    }
+}
