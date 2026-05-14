3296d2526da09f53deeb60cd655198c1ebdd5624
ellensalua <elensalua@gmail.com>
2026-05-14 19:56:30 +0800

    chore: mengisi code dalam file gedungmodel.php

--- app/Models/GedungModel.php
+++ app/Models/GedungModel.php
@@ -1,1 +1,20 @@
+<?php
 
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class GedungModel extends Model
+{
+    protected $table = 'gedung';
+    protected $primaryKey = 'id';
+    protected $allowedFields = [
+        'nama_gedung',
+        'kapasitas',
+        'harga',
+        'harga_per_jam',
+        'durasi_sewa',
+        'lokasi',
+        'status'
+    ];
+}
