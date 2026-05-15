1942ba1506fe6a38461c284a86a89394d4090530
adhitsi <f52124062@untad.ac.id>
2026-05-15 14:36:11 +0800

    Reset route

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,17 +7,3 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 
-// Auth
-$routes->get('/login', 'Auth::login');
-$routes->post('/login', 'Auth::loginProcess');
-$routes->get('/register', 'Auth::register');
-$routes->post('/register', 'Auth::registerProcess');
-$routes->get('/logout', 'Auth::logout');
-
-// Dashboard
-$routes->get('/dashboard', 'Dashboard::index');
-$routes->get('/dashboard/create', 'Dashboard::create');
-$routes->post('/dashboard/store', 'Dashboard::store');
-$routes->get('/dashboard/edit/($id)', 'Dashboard::edit/$id');
-$routes->post('/dashboard/update/(:$id)', 'Dashboard::update/$id');
-$routes->get('/dashboard/delete/(:$id)', 'Dashboard::delete/$id');
