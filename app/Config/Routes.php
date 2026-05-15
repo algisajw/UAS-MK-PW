5c306d5a171012c901c2dd557dbf4999cbb3cd0b
adhitsi <f52124062@untad.ac.id>
2026-05-15 14:21:11 +0800

    Menambahkan route Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,18 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+// Auth
+$routes->get('/login', 'Auth::login');
+$routes->post('/login', 'Auth::loginProcess');
+$routes->get('/register', 'Auth::register');
+$routes->post('/register', 'Auth::registerProcess');
+$routes->get('/logout', 'Auth::logout');
+
+// Dashboard
+$routes->get('/dashboard', 'Dashboard::index');
+$routes->get('/dashboard/create', 'Dashboard::create');
+$routes->post('/dashboard/store', 'Dashboard::store');
+$routes->get('/dashboard/edit/($id)', 'Dashboard::edit/$id');
+$routes->post('/dashboard/update/(:$id)', 'Dashboard::update/$id');
+$routes->get('/dashboard/delete/(:$id)', 'Dashboard::delete/$id');
