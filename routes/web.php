c8ff441e9ca63beedc4996ca260acb81d7f6bf99
Algis Asmaradhana JW <gisssss.jw@gmail.com>
2026-06-02 21:46:07 +0800

    Add authentication routes and middleware

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\GedungController;
+use App\Http\Controllers\PenyewanController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('guest')->group(function () {
+    Route::get('/', [AuthController::class, 'showLogin'])->name('index');
+    Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+    Route::post('/register', [AuthController::class, 'register']);
+});
+
+Route::middleware('auth')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::resource('gedung', GedungController::class);
+    Route::resource('penyewan', PenyewanController::class);
 });
