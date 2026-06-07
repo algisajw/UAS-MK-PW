d551497b0ce191baf6467183538ea6136ddf5cab
Algis Asmaradhana JW <gisssss.jw@gmail.com>
2026-06-07 23:43:52 +0800

    Add minimum duration validation for rental days

--- app/Http/Controllers/PenyewanController.php
+++ app/Http/Controllers/PenyewanController.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http/Controllers;
 
 use App\Models\Penyewan;
 use App\Models\Gedung;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class PenyewanController extends Controller
 {
@@ -26,8 +27,30 @@ class PenyewanController extends Controller
             'gedung_id' => 'required|exists:gedungs,id',
             'nama_penyewa' => 'required',
             'tanggal_sewa' => 'required|date',
-            'durasi_hari' => 'required|integer',
+            'durasi_hari' => 'required|integer|min:1',
         ]);
+
+        $inputMulai = $request->tanggal_sewa;
+        $inputSelesai = Carbon::parse($inputMulai)
+            ->addDays($request->durasi_hari - 1)
+            ->toDateString();
+
+        $bentrok = Penyewan::where('gedung_id', $request->gedung_id)
+            ->where(function ($query) use ($inputMulai, $inputSelesai) {
+                $query->whereBetween('tanggal_sewa', [$inputMulai, $inputSelesai])
+                    ->orWhereRaw('DATE_ADD(tanggal_sewa, INTERVAL (durasi_hari - 1) DAY) BETWEEN ? AND ?', [$inputMulai, $inputSelesai])
+                    ->orWhere(function ($q) use ($inputMulai, $inputSelesai) {
+                        $q->where('tanggal_sewa', '<=', $inputMulai)
+                            ->whereRaw('DATE_ADD(tanggal_sewa, INTERVAL (durasi_hari - 1) DAY) >= ?', [$inputSelesai]);
+                    });
+            })
+            ->exists();
+
+        if ($bentrok) {
+            return back()
+                ->withInput()
+                ->withErrors(['tanggal_sewa' => 'Gedung sudah dipesan pada rentang tanggal tersebut.']);
+        }
 
         $gedung = Gedung::findOrFail($request->gedung_id);
         $total_harga = $gedung->harga_sewa * $request->durasi_hari;
@@ -55,8 +78,31 @@ class PenyewanController extends Controller
             'gedung_id' => 'required|exists:gedungs,id',
             'nama_penyewa' => 'required',
             'tanggal_sewa' => 'required|date',
-            'durasi_hari' => 'required|integer',
+            'durasi_hari' => 'required|integer|min:1',
         ]);
+
+        $inputMulai = $request->tanggal_sewa;
+        $inputSelesai = Carbon::parse($inputMulai)
+            ->addDays($request->durasi_hari - 1)
+            ->toDateString();
+
+        $bentrok = Penyewan::where('gedung_id', $request->gedung_id)
+            ->where('id', '!=', $penyewan->id)
+            ->where(function ($query) use ($inputMulai, $inputSelesai) {
+                $query->whereBetween('tanggal_sewa', [$inputMulai, $inputSelesai])
+                    ->orWhereRaw('DATE_ADD(tanggal_sewa, INTERVAL (durasi_hari - 1) DAY) BETWEEN ? AND ?', [$inputMulai, $inputSelesai])
+                    ->orWhere(function ($q) use ($inputMulai, $inputSelesai) {
+                        $q->where('tanggal_sewa', '<=', $inputMulai)
+                            ->whereRaw('DATE_ADD(tanggal_sewa, INTERVAL (durasi_hari - 1) DAY) >= ?', [$inputSelesai]);
+                    });
+            })
+            ->exists();
+
+        if ($bentrok) {
+            return back()
+                ->withInput()
+                ->withErrors(['tanggal_sewa' => 'Gedung sudah dipesan pada rentang tanggal tersebut.']);
+        }
 
         $gedung = Gedung::findOrFail($request->gedung_id);
         $total_harga = $gedung->harga_sewa * $request->durasi_hari;
